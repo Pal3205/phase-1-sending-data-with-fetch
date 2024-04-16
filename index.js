43689e31879ec5cd333f711a6db17147cb061529
Pebble <pal19carson.m@gmai.com>
2024-04-16 23:37:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const newId = document.createElement("p");
+        newId.textContent = data.id;
+        document.body.appendChild(newId);
+      })
+      .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+      });
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+
+  });
+  
